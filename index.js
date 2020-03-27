306ab071f91342b79d5ac094eba13b497c539860
Joe Sweeney <joe.sweeney224@gmail.com>
2020-03-26 19:39:19 -0600

    Better variable

--- index.js
+++ index.js
@@ -5,51 +5,52 @@ let _ = require('lodash')
 module.exports = {
   async parseFeed(rawFeed) {
     let parsedFeed = await xml2js.parseStringPromise(rawFeed)
+    let podcastData = parsedFeed.rss.channel[0]
     let podcastJSON = {}
 
     podcastJSON.meta = {}
 
     // Title
-    if (parsedFeed.rss.channel[0].hasOwnProperty('title')) {
-      podcastJSON.meta.title = parsedFeed.rss.channel[0].title[0]
+    if (podcastData.hasOwnProperty('title')) {
+      podcastJSON.meta.title = podcastData.title[0]
     }
 
     // Description
-    if (parsedFeed.rss.channel[0].hasOwnProperty('description')) {
-      podcastJSON.meta.description = parsedFeed.rss.channel[0].description[0]
+    if (podcastData.hasOwnProperty('description')) {
+      podcastJSON.meta.description = podcastData.description[0]
     }
 
     // Image
-    if (parsedFeed.rss.channel[0].hasOwnProperty('itunes:image')) {
-      podcastJSON.meta.imageURL = parsedFeed.rss.channel[0]['itunes:image'][0]['$'].href
+    if (podcastData.hasOwnProperty('itunes:image')) {
+      podcastJSON.meta.imageURL = podcastData['itunes:image'][0]['$'].href
     }
 
     // Link
-    if (parsedFeed.rss.channel[0].hasOwnProperty('link')) {
-      podcastJSON.meta.link = parsedFeed.rss.channel[0].link[0]
+    if (podcastData.hasOwnProperty('link')) {
+      podcastJSON.meta.link = podcastData.link[0]
     }
 
     // Language
-    if (parsedFeed.rss.channel[0].hasOwnProperty('language')) {
-      podcastJSON.meta.language = parsedFeed.rss.channel[0].language[0]
+    if (podcastData.hasOwnProperty('language')) {
+      podcastJSON.meta.language = podcastData.language[0]
     }
 
     // Author
-    if (parsedFeed.rss.channel[0].hasOwnProperty('itunes:author')) {
-      podcastJSON.meta.author = parsedFeed.rss.channel[0]['itunes:author'][0]
+    if (podcastData.hasOwnProperty('itunes:author')) {
+      podcastJSON.meta.author = podcastData['itunes:author'][0]
     }
 
     // Summary
-    if (parsedFeed.rss.channel[0].hasOwnProperty('itunes:summary')) {
-      podcastJSON.meta.summary = parsedFeed.rss.channel[0]['itunes:summary'][0]
+    if (podcastData.hasOwnProperty('itunes:summary')) {
+      podcastJSON.meta.summary = podcastData['itunes:summary'][0]
     }
 
     // Categories
-    if (parsedFeed.rss.channel[0].hasOwnProperty('itunes:category')) {
+    if (podcastData.hasOwnProperty('itunes:category')) {
       podcastJSON.meta.categories = []
       let categoriesRaw = []
 
-      for (let upperLevelCategory of parsedFeed.rss.channel[0]['itunes:category']) {
+      for (let upperLevelCategory of podcastData['itunes:category']) {
         // Upper level details
         categoriesRaw.push(upperLevelCategory['$'].text)
 
@@ -65,37 +66,37 @@ module.exports = {
     }
 
     // Owner
-    if (parsedFeed.rss.channel[0].hasOwnProperty('itunes:owner')) {
+    if (podcastData.hasOwnProperty('itunes:owner')) {
       podcastJSON.meta.owner = {}
 
-      if (parsedFeed.rss.channel[0]['itunes:owner'][0].hasOwnProperty('itunes:name')) {
-        podcastJSON.meta.owner.name = parsedFeed.rss.channel[0]['itunes:owner'][0]['itunes:name'][0]
+      if (podcastData['itunes:owner'][0].hasOwnProperty('itunes:name')) {
+        podcastJSON.meta.owner.name = podcastData['itunes:owner'][0]['itunes:name'][0]
       }
 
-      if (parsedFeed.rss.channel[0]['itunes:owner'][0].hasOwnProperty('itunes:email')) {
-        podcastJSON.meta.owner.email = parsedFeed.rss.channel[0]['itunes:owner'][0]['itunes:email'][0]
+      if (podcastData['itunes:owner'][0].hasOwnProperty('itunes:email')) {
+        podcastJSON.meta.owner.email = podcastData['itunes:owner'][0]['itunes:email'][0]
       }
     }
 
     // Explicit?
-    if (parsedFeed.rss.channel[0].hasOwnProperty('itunes:explicit')) {
+    if (podcastData.hasOwnProperty('itunes:explicit')) {
       podcastJSON.meta.explicit = false
 
-      if (['yes', 'explicit', 'true'].indexOf(parsedFeed.rss.channel[0]['itunes:explicit'][0].toLowerCase()) >= 0) {
+      if (['yes', 'explicit', 'true'].indexOf(podcastData['itunes:explicit'][0].toLowerCase()) >= 0) {
         podcastJSON.meta.explicit = true
       }
     }
 
     // Pages
-    if (parsedFeed.rss.channel[0].hasOwnProperty('atom:link')) {
+    if (podcastData.hasOwnProperty('atom:link')) {
       podcastJSON.meta.pages = {}
 
-      for (let page of parsedFeed.rss.channel[0]['atom:link']) {
+      for (let page of podcastData['atom:link']) {
         podcastJSON.meta.pages[page['$'].rel] = page['$'].href
       }
     }
 
-    // console.log(parsedFeed.rss.channel[0]['itunes:category'])
+    // console.log(podcastData['itunes:category'])
 
     return podcastJSON
   }
